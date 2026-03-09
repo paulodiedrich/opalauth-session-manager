24a94fcd9dac33f2e702c6f3fb19f8dcc052faa1
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-03-09 11:52:26 -0300

    feat: Simulação de hash.

--- js/login.js
+++ js/login.js
@@ -11,9 +11,13 @@ function validaCampo(){
     //Capturando os dados do campo SENHA:
     const campoSenha = document.getElementById("input-senha");
     let valorCampo = parseInt(campoSenha.value);
-    
+
     //Verificando se a senha é PAR
     console.log(valorCampo % 2 == 0);
+
+    //Simulação de HASH
+    const valorHash = valorCampo ** 2;
+    console.log(valorHash);
 }
 
 
