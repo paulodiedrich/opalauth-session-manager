cdb7ab08b74d347387557e9272ddafa3d017c7f2
GuilhermeAnitelli <guilherme.anitelli10@gmail.com>
2026-03-09 11:27:20 -0300

    feat: Conversão de tipos e captura do campo senha.

---
+++ js/login.js
@@ -0,0 +1,17 @@
+
+const botaoEnviar =document.querySelector("#btn-enviar");
+
+// botaoenviar.addEventListener("click", function() {
+//     console.log("TESTE")
+// });
+
+botaoEnviar.addEventListener("click", validaCampo);
+
+function validaCampo() {
+    // Capturando os dados do campo SENHA:
+    const campoSenha = document.getElementById("input-senha");
+    let valorCampo = parseInt(campoSenha.value);
+
+// Verificando se a senha é PAR:
+console.log(valorCampo % 2 == 0);
+}
