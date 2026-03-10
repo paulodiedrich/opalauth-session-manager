535f5f53ee8b90dd0f8a1dddf58f7fe5eb515f6d
manuellarinaldi <manuellarinaldi@gmail.com>
2026-03-10 10:48:56 -0300

    feat: Criamos o botão Enviar, capturamos os dados do campo senha e convertemos em String;

--- js/login.js
+++ js/login.js
@@ -4,13 +4,20 @@ const botaoEnviar = document.querySelector("#btn-entrar");
 botaoEnviar.addEventListener("click", validaCampo);
 
 function validaCampo(){
+    //Capturando os dados do campo SENHA:
     const campoSenha = document.getElementById("input-senha");
     const valorCampo = Number(campoSenha.value);
-
+     
+    //Verifica se o valor é um número válido antes de prosseguir
     if(Number.isNaN(valorCampo)){
         console.log9("Valor de senha inválido.");
         return;
     }
 
-    console.log
+    //Verificando se a senha é PAR:
+    console.log("resultado:", valorCampo % 2 == 0);
+ 
+    //Simulação de HASH:
+    const valorHash = valorCampo **2;
+    console.log("Senha modificada:", valorHash);
 }
